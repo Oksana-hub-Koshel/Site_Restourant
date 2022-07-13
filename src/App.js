95f7be745bf067af3b56f361b094c0596c53360f
Oksana Koshel <oksana.koshel@ukr.net>
2022-07-13 12:55:14 +0300

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import { Component } from 'react';
+import Menu from './components/MenuComponent';
 
 
 class  App extends  Component {
   render(){
     return (
-   <div className='App'>
+   <div>
     <Navbar dark color="primary">
       <div className='container'>
         <NavbarBrand href='/ '>Ristorante Con fusion</NavbarBrand>
       </div>
     </Navbar>
+    <Menu />
    </div>
       );
   }
